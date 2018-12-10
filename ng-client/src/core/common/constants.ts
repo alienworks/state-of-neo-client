e93182b6f145beaeea8258ff40cfc3d3b03ade76
yasenm <ias_sv_bg@yahoo.co.uk>
2018-12-10 11:14:36 +0200

    changed server to new server to use

--- ng-client/src/core/common/constants.ts
+++ ng-client/src/core/common/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://45.76.43.23';
+export const BASE_URL = 'http://151.106.58.158';
 
 export const MainThemeColor = '#079098';
 export const ColorPalet = ['#079098', '#cad04a', '#456177', '#869514', '#e3e7e8', '#272c2f', '#6b6e73', '#e3e7e8', '#6AF9C4'];
