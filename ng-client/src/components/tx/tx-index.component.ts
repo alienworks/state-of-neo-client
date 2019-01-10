7b54fb8abf8ccb4ab8e7b8ebe8801559380df408
yasenm <ias_sv_bg@yahoo.co.uk>
2019-01-10 14:27:20 +0200

    tx total count for tx-index fixed

--- ng-client/src/components/tx/tx-index.component.ts
+++ ng-client/src/components/tx/tx-index.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { PageResultModel, BaseTxModel } from '../../models';
 import { TxService } from '../../core/services/data';
 import { CommonStateService } from '../../core/services';
+import { StatsSignalRService } from 'src/core/services/signal-r';
 
 @Component({
     templateUrl: './tx-index.component.html'
@@ -9,15 +10,25 @@ import { CommonStateService } from '../../core/services';
 export class TxIndexComponent implements OnInit {
     transactions: PageResultModel<BaseTxModel>;
     count: number;
+    claimedGas: number;
+    totalClaimedUpdate = new EventEmitter<number>();
 
-    constructor(private txService: TxService,
-        private state: CommonStateService
-    ) { }
+    constructor(
+        private txService: TxService,
+        private state: CommonStateService,
+        private statsSrService: StatsSignalRService) { }
 
     ngOnInit() {
         this.state.changeRoute('transactions');
 
         this.getPage(1);
+
+        this.statsSrService.registerAdditionalEvent('total-claimed', this.totalClaimedUpdate);
+        this.totalClaimedUpdate.subscribe((x: number) => {
+            return this.claimedGas = x;
+        });
+
+        this.statsSrService.invokeOnServerEvent(`InitInfo`, 'caller');
     }
 
     getPage(page: number): void {
