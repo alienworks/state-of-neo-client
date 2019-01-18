a2afa1e108cbc25481cb5d7c2efcd94aafd3a87a
Zdravko Georgiev <skinsomnia@gmail.com>
2019-01-18 11:00:33 +0200

    Add transactions filter by type

--- ng-client/src/components/tx/tx-index.component.ts
+++ ng-client/src/components/tx/tx-index.component.ts
@@ -9,6 +9,20 @@ import { CommonStateService } from '../../core/services';
 export class TxIndexComponent implements OnInit {
     transactions: PageResultModel<BaseTxModel>;
     count: number;
+    type: string = null;
+    txTypes: any = [
+        { label: 'Miner', value: 0 },
+        { label: 'Issue', value: 1 },
+        { label: 'Claim', value: 2 },
+        { label: 'Enroll', value: 32 },
+        { label: 'Register', value: 64 },
+        { label: 'Contract', value: 128 },
+        { label: 'State', value: 144 },
+        { label: 'Publish', value: 208 },
+        { label: 'Invocation', value: 209 },
+        
+    ];
+    page: number = 1;
 
     constructor(private txService: TxService,
         private state: CommonStateService
@@ -17,11 +31,12 @@ export class TxIndexComponent implements OnInit {
     ngOnInit() {
         this.state.changeRoute('transactions');
 
-        this.getPage(1);
+        this.getPage(this.page);
     }
 
     getPage(page: number): void {
-        this.txService.getPage(page, 14)
+        console.log(this.type);
+        this.txService.getPage(page, 14, null, null, null, this.type)
             .subscribe(x => {
                 this.transactions = x.json() as PageResultModel<BaseTxModel>;
             }, err => {
