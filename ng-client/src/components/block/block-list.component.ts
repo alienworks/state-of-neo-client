23f2778a3c1383dbbd5810eb73ec23eb353a4161
yasenm <ias_sv_bg@yahoo.co.uk>
2019-01-19 17:56:46 +0200

    fade in of new block added

--- ng-client/src/components/block/block-list.component.ts
+++ ng-client/src/components/block/block-list.component.ts
@@ -1,29 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { BlockService } from '../../core/services/data/block.service';
 import { CommonStateService } from '../../core/services/';
 import { BlockListModel } from '../../models/block.models';
-import { PageResultModel } from '../../models';
+import { PageResultModel, HeaderInfoModel } from '../../models';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { StatsSignalRService } from 'src/core/services/signal-r';
 
 @Component({
-    templateUrl: `./block-list.component.html`
+    templateUrl: `./block-list.component.html`,
+    animations: [
+        trigger('fadeIn', [
+            transition(':enter', [
+                style({ opacity: '0' }),
+                animate('.5s ease-in', style({ opacity: '1' }))
+            ])
+        ])
+    ]
 })
 export class BlockListComponent implements OnInit {
     pageResults: PageResultModel<BlockListModel>;
     isLoading = true;
+    headerUpdate = new EventEmitter<HeaderInfoModel>();
 
-    constructor(private _blockService: BlockService,
-        private state: CommonStateService
-    ) { }
+    constructor(
+        private blockService: BlockService,
+        private state: CommonStateService,
+        private statsSrService: StatsSignalRService) { }
 
     ngOnInit(): void {
         this.getPage(1);
+
+        this.statsSrService.registerAdditionalEvent('header', this.headerUpdate);
+        this.headerUpdate.subscribe((x: HeaderInfoModel) => {
+            if (this.pageResults.items.findIndex(b => b.height === x.height) === -1) {
+                const newBlock = new BlockListModel();
+                newBlock.height = x.height;
+                newBlock.transactionsCount = x.transactionCount;
+                newBlock.timestamp = x.timestamp;
+                newBlock.size = x.size;
+
+                this.pageResults.items.pop();
+                this.pageResults.items.unshift(newBlock);
+            }
+        });
     }
 
     getPage(page: number): void {
         this.state.changeRoute('blocks');
 
         this.isLoading = true;
-        this._blockService.getBlocksPage(page, 32)
+        this.blockService.getBlocksPage(page, 32)
             .subscribe(x => {
                 this.pageResults = x.json() as PageResultModel<BlockListModel>;
                 this.isLoading = false;
