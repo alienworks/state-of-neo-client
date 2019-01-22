8ec3cfde55d7088ce62a6f68d6ec9e9d9642bdf5
Zdravko Georgiev <skinsomnia@gmail.com>
2019-01-22 08:56:43 +0200

    Home block UX improvements

--- ng-client/src/components/home-block.component.ts
+++ ng-client/src/components/home-block.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: `app-home-block`,
-    templateUrl: `./home-block.component.html`
+    templateUrl: `./home-block.component.html`,
+    styleUrls: [
+        './home-block.component.css'
+    ]
 })
 export class HomeBlockComponent  {
     model = {
