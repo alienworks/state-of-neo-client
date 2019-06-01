08e429cac6ce7ca3308b45df0a08f5a20e37ecd8
yasenm <ias_sv_bg@yahoo.co.uk>
2019-06-01 23:55:37 +0300

    app-loader more configurable

--- ng-client/src/components/common/loader.component.ts
+++ ng-client/src/components/common/loader.component.ts
@@ -7,4 +7,6 @@ import { Component, Input } from '@angular/core';
 export class LoaderComponent {
     @Input() title: string;
     @Input() iconScale = 4;
+    @Input() textPosition = "text-center";
+    @Input() padding = 10;
 }
