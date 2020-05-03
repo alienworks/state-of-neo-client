729812fa519b5778f708798ffa2cb32628f9c7ef
AmagiDDmxh <22388514+AmagiDDmxh@users.noreply.github.com>
2020-05-03 19:23:32 +0800

    Update spinner to center properly, remove unused http ngmodule

--- ng-client/src/app/app.module.ts
+++ ng-client/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { PreloadAllModules, RouterModule } from '@angular/router';
 import { DatePipe } from '@angular/common';
@@ -92,7 +91,6 @@ import { BlockIndexComponent } from '../components/block/block-index.component';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    HttpModule,
     FormsModule,
     NgxPaginationModule,
     SelectDropDownModule,
